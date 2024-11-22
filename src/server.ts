0d8c8b915151f488e8f128d07a3fb2fa576af172
Ahmed Hany <ah7608867@gmail.com>
2024-11-22 03:06:11 +0200

    build the srever [init]

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+const mongo_sanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const helmet = require('helmet')
+const limitRate = require('express-rate-limit')
+
+import { AppRouter } from "./app";
+import { connectDB } from './DBconnection';
+
+dotenv.config({ path: 'config.env' });
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev')); // for logging requests in console 
+app.use(express.json({})); // for parsing application/json
+app.use(mongo_sanitize()); // for preventing nosql injection
+app.use(xss()); // for preventing xss attacks
+app.use(helmet()); // for setting security headers
+app.use(limitRate({ windowMs: 60 * 60 * 1000, max: 100, message: 'Too many requests' })) // for limiting requests
+
+app.get('/', (req, res) => {
+    res.send('Hello from express');
+});
+
+
+app.use('/ecommerce', AppRouter.getInstance());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server running on port ${PORT} hit http://localhost:${PORT}`);
+});
