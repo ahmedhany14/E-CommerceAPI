794c7604ff3a38c0fab1d46e2a252383bfa4fa2c
Ahmed Hany <ah7608867@gmail.com>
2024-11-22 14:55:22 +0200

    Creating Acount Schema

---
+++ src/Models/Account/entitie/account-entite.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+export interface AccountEntite extends mongoose.Document {
+    email: string;
+    password: string;
+    confirmPassword: string;
+    passwordChangedTime: Date;
+    resetToken: string;
+    expireResetToken: Date;
+}
+
+const accountSchema: mongoose.Schema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8
+    },
+    confirmPassword: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            validator: function (this: AccountEntite): boolean {
+                return this.password === this.confirmPassword;
+            },
+            message: 'Passwords are not the same'
+        }
+    },
+    passwordChangedTime: Date,
+    resetToken: String,
+    expireResetToken: Date
+});
+
+const Account = mongoose.model<AccountEntite>('Account', accountSchema);
+
+export default Account;
