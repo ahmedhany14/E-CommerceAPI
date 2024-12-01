99ece0bd36d2ecceb28776af2cbf256a9b6bda17
Ahmed Hany <ah7608867@gmail.com>
2024-12-02 01:22:05 +0200

    refactor[server]: refactor error types and make more flexible

--- src/Common/utils/AppError.ts
+++ src/Common/utils/AppError.ts
@@ -1,9 +1,31 @@
 
-class AppError extends Error {
+export class AppError extends Error {
     constructor(public message: string, public statusCode: number) {
         super(message);
         this.statusCode = statusCode;
     }
 }
 
-export { AppError };
+export class AuthError extends AppError {
+	constructor(message: string, statusCode: number = 401) {
+		super(message, statusCode);
+	}
+}
+
+export class ValidationError extends AppError {
+	constructor(message: string, statusCode: number = 400) {
+		super(message, statusCode);
+	}
+}
+
+export class NotFoundError extends AppError {
+	constructor(message: string, statusCode: number = 404) {
+		super(message, statusCode);
+	}
+}
+
+export class BadRequestError extends AppError {
+	constructor(message: string, statusCode: number = 400) {
+		super(message, statusCode);
+	}
+}
